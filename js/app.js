515cd9e3210e8114005ddab12b1d463547cba5b5
danieltocado <danieltocado2@gmail.com>
2020-05-31 20:10:50 +0200

    Calculadora casi hecha

---
+++ js/app.js
@@ -0,0 +1,131 @@
+var operando1;
+var opreando2;
+var operacion;
+
+
+
+function init () {
+
+    //Definir variables
+    var resultado = document.getElementById("resultado");
+
+        //Numeros
+    var num1 = document.getElementById("num1");
+    var num2 = document.getElementById("num2");
+    var num3 = document.getElementById("num3");
+    var num4 = document.getElementById("num4");
+    var num5 = document.getElementById("num5");
+    var num6 = document.getElementById("num6");
+    var num7 = document.getElementById("num7");
+    var num8 = document.getElementById("num8");
+    var num9 = document.getElementById("num9");
+    var num0 = document.getElementById("num0");
+
+        //Operaciones
+    var sumar = document.getElementById("sumar");
+    var restar = document.getElementById("restar");
+    var multiplicar = document.getElementById("multiplicar");
+    var dividir = document.getElementById("dividir");
+    var modulo = document.getElementById("modulo");
+
+        //Resultado
+    var resultadofinal = document.getElementById("resultadofinal");
+
+    //Mostrar números en resultado
+    num1.onclick = function(e){
+        resultado.textContent = resultado.textContent + "1";
+    }
+    num2.onclick = function(e){
+        resultado.textContent = resultado.textContent + "2";
+    }
+    num3.onclick = function(e){
+        resultado.textContent = resultado.textContent + "3";
+    }
+    num4.onclick = function(e){
+        resultado.textContent = resultado.textContent + "4";
+    }
+    num5.onclick = function(e){
+        resultado.textContent = resultado.textContent + "5";
+    }
+    num6.onclick = function(e){
+        resultado.textContent = resultado.textContent + "6";
+    }
+    num7.onclick = function(e){
+        resultado.textContent = resultado.textContent + "7";
+    }
+    num8.onclick = function(e){
+        resultado.textContent = resultado.textContent + "8";
+    }
+    num9.onclick = function(e){
+        resultado.textContent = resultado.textContent + "9";
+    }
+    num0.onclick = function(e){
+        resultado.textContent = resultado.textContent + "0";
+    }
+
+    
+    // Operaciones
+    sumar.onclick = function(e){
+        operando1 = resultado.textContent;
+        operacion = "+";
+        limpiar();
+    }
+
+    restar.onclick = function(e){
+        operando1 = resultado.textContent;
+        operacion = "-";
+        limpiar();
+    }
+
+    multiplicar.onclick = function(e){
+        operando1 = resultado.textContent;
+        operacion = "*";
+        limpiar();
+    }
+
+    dividir.onclick = function(e){
+        operando1 = resultado.textContent;
+        operacion = "/";
+        limpiar();
+    }
+
+    modulo.onclick = function(e){
+        operando1 = resultado.textContent;
+        operacion = "%";
+        limpiar();
+    }
+
+    resultadofinal.onclick = function(e){
+        operando2 = resultado.textContent;
+        resolver();
+    }
+}
+
+function limpiar(){
+    resultado.textContent = "";
+}
+
+
+
+function resolver() {
+    var res = 0;
+    switch(operacion){
+        case "+":
+            res = Number(operando1) + Number(operando2);
+            break;
+        case "-":
+            res = Number(operando1) - Number(operando2);
+            break;
+        case "*":
+            res = Number(operando1) * Number(operando2);
+            break;  
+        case "/":
+            res = Number(operando1) / Number(operando2);
+            break;     
+        case "%":
+            res = Number(operando1) % Number(operando2);
+            break;                 
+    }
+    
+    resultado.textContent = res;
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,129 @@
+// Obtener números y operandos desde HTML
 
-function creaNodo(){
-    //empezamos a crear html cada vez que el botón se pulse y por lo tanto ejecutamos esta función
+document.getElementById("num1").addEventListener("click",num1);
+document.getElementById("num2").addEventListener("click",num2);
+document.getElementById("num3").addEventListener("click",num3);
+document.getElementById("num4").addEventListener("click",num4);
+document.getElementById("num5").addEventListener("click",num5);
+document.getElementById("num6").addEventListener("click",num6);
+document.getElementById("num7").addEventListener("click",num7);
+document.getElementById("num8").addEventListener("click",num8);
+document.getElementById("num9").addEventListener("click",num9);
+document.getElementById("num0").addEventListener("click",num0);
 
-    let texto = document.createTextNode("Hola estamos en clase.");
-    let p = document.createElement("p");
-    p.appendChild(texto);
+document.getElementById("sumar").addEventListener("click",signoSuma);
+document.getElementById("restar").addEventListener("click",signoResta);
+document.getElementById("dividir").addEventListener("click",signoDivision);
+document.getElementById("multiplicar").addEventListener("click",signoMultiplicacion);
+document.getElementById("limpiar").addEventListener("click",limpiar);
 
-    document.getElementById("contenedor").appendChild(p);
-    console.log(texto);
+document.getElementById("resultado").maxLength = "14";
+
+
+// Mostrar números en pantalla
+
+function num1() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("num1").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function num2() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("num2").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function num3() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("num3").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function num4() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("num4").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function num5() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("num5").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function num6() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("num6").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function num7() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("num7").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function num8() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("num8").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function num9() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("num9").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function num0() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("num0").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+// Mostrar operandos en pantalla
+
+function signoSuma() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("sumar").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function signoResta() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("restar").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function signoDivision() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("dividir").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+function signoMultiplicacion() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("multiplicar").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+}
+
+// Funciones operaciones
+
+function sumar() {
+    let resultadoactual = document.getElementById("resultado").innerHTML;
+    let printNum = document.getElementById("multiplicar").innerHTML;
+    document.getElementById("resultado").innerHTML = resultadoactual + printNum;
+    
+
 }
 
 
+
+
+
+function limpiar(){
+    
+    document.getElementById("resultado").innerHTML = "";
+   
+} 
